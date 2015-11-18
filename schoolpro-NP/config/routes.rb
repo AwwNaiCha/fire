fd4f30de0f77b6cd239e5e789e2a5ed3004c57d8
HuiminJian <jianhuimin89@gmail.com>
2015-11-17 21:49:57 -0800

    gem devise added

--- schoolpro-NP/config/routes.rb
+++ schoolpro-NP/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'provinces#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :maps
+  get 'home' => 'provinces#index'
+
   resources :provinces
   # Example resource route with options:
   #   resources :products do
